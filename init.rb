13ca68a7a99764c9d36633b51f1b7ae296a0c154
Michał Lipski <michal@tallica.pl>
2017-12-02 20:31:41 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,32 +1,24 @@
-# Rails plugin initialization.
-
 require 'recaptcha'
-require 'net/http'
 require 'recaptcha/rails'
-require 'redmine'
-require 'dispatcher'
 
-Dispatcher.to_prepare :redmine_recaptcha do
-  require_dependency 'recaptcha/client_helper'
-  require_dependency 'client_helper_patch'
-  Recaptcha::ClientHelper.send(:include, ClientHelperPatch) 
-
+ActionDispatch::Callbacks.to_prepare do
   require_dependency 'account_controller'
   require_dependency 'account_controller_patch'
-  AccountController.send(:include, AccountControllerPatch) 
+  AccountController.send(:include, AccountControllerPatch)
 end
 
-Redmine::Plugin.register :redmine_recaptcha do
-  name 'reCAPTCHA for user self registration'
-  author 'Shane StClair'
-  description 'Adds a recaptcha to the user self registration screen to combat spam'
-  version '0.1.0'
-  url 'http://github.com/srstclair/redmine_recaptcha'
-  #requires_redmine :version_or_higher => '0.9.0'
-  settings :default => {
-     'recaptcha_private_key' => '',
-     'recaptcha_public_key' => ''
-  }, :partial => 'settings/redmine_recaptcha'
-
+Redmine::Plugin.register :redmine_recaptcha_v2 do
+  name 'Redmine reCAPTCHA v2 plugin'
+  author 'Shane StClair and Michał Lipski'
+  description "Adds a reCAPTACHA to Redmine's user self registration screen to combat spam"
+  version '0.2.0'
+  url 'https://github.com/tallica/redmine_recaptcha_v2'
+  requires_redmine :version_or_higher => '3.4.0'
+  settings(
+    :default => {
+      'site_key' => '',
+      'secret_key' => ''
+    },
+    :partial => 'settings/redmine_recaptcha_v2'
+  )
 end
-
