7076bda0224489f6141c2fe2dd7de8a351a1a9ae
Michał Lipski <michal@tallica.pl>
2017-12-02 20:37:09 +0100

    Update account controller patch

--- lib/account_controller_patch.rb
+++ lib/account_controller_patch.rb
@@ -3,19 +3,22 @@ module AccountControllerPatch
     base.send(:include, InstanceMethods)
 
     base.class_eval do
+      unloadable
       alias_method_chain :register, :recaptcha_verification
     end
   end
-  
+
   module InstanceMethods
     def register_with_recaptcha_verification
-      redirect_to(home_url) && return unless Setting.self_registration? || session[:auth_source_registration]
-
-      if request.get?
+      (redirect_to(home_url); return) unless Setting.self_registration? || session[:auth_source_registration]
+      if !request.post?
         session[:auth_source_registration] = nil
-        @user = User.new(:language => Setting.default_language)
+        @user = User.new(:language => current_language.to_s)
       else
-        @user = User.new(params[:user])
+        user_params = params[:user] || {}
+        @user = User.new
+        @user.safe_attributes = user_params
+        @user.pref.safe_attributes = params[:pref]
         @user.admin = false
         @user.register
         if session[:auth_source_registration]
@@ -26,20 +29,26 @@ module AccountControllerPatch
             session[:auth_source_registration] = nil
             self.logged_user = @user
             flash[:notice] = l(:notice_account_activated)
-            redirect_to :controller => 'my', :action => 'account'
+            redirect_to my_account_path
           end
         else
-          @user.login = params[:user][:login]
-          @user.password, @user.password_confirmation = params[:password], params[:password_confirmation]
-          if verify_recaptcha( :private_key => Setting.plugin_redmine_recaptcha['recaptcha_private_key'], :model => @user, :message => "There was an error with the recaptcha code below. Please re-enter the code and click submit." )
-            case Setting.self_registration
-            when '1'
-              register_by_email_activation(@user)
-            when '3'
-              register_automatically(@user)
-            else
-              register_manually_by_administrator(@user)
-            end
+          unless user_params[:identity_url].present? && user_params[:password].blank? && user_params[:password_confirmation].blank?
+            @user.password, @user.password_confirmation = user_params[:password], user_params[:password_confirmation]
+          end
+
+          return unless
+            verify_recaptcha(
+              :secret_key => Setting.plugin_redmine_recaptcha_v2['secret_key'],
+              :model => @user
+            )
+
+          case Setting.self_registration
+          when '1'
+            register_by_email_activation(@user)
+          when '3'
+            register_automatically(@user)
+          else
+            register_manually_by_administrator(@user)
           end
         end
 
